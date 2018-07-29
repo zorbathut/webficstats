a5de035c8f020fd65195742a9c9b4f3d66462b99
Ben Rog-Wilhelm <zorba-ubuntudev-20180724@pavlovian.net>
2018-07-29 13:07:55 -0700

    Lots of labels on the per-week graph

--- render.py
+++ render.py
@@ -4,7 +4,6 @@ from disk import *
 import svgwrite
 import util_math
 import datetime
-import dateutil.parser
 
 def render():
     render_lengths()
@@ -47,7 +46,11 @@ def render_lengths():
             dwg.defs.add(gradient)
 
             dwg.add(dwg.rect((start[0] + size[0], start[1] + 1), (20, size[1] - 2), fill = gradient.get_paint_server()))
-        dwg.add(dwg.text(story.name, insert = (-5, (idx + 0.8) * columnheight), text_anchor = 'end', font_size = 10, alignment_baseline = 'middle'))
+        dwg.add(dwg.text(story.name,
+            insert = (-5, (idx + 0.8) * columnheight),
+            text_anchor = 'end',
+            font_size = 10,
+            alignment_baseline = 'middle'))
     dwg.save()
 
 def render_words_per_week():
@@ -65,7 +68,7 @@ def render_words_per_week():
     textwidth = 150
     imageborder = 20
 
-    ul = (-imageborder, -imageborder)
+    ul = (-imageborder - 50, -imageborder - 10)
     br = (width + textwidth + imageborder, height + imageborder)
     size = (br[0] - ul[0], br[1] - ul[1])
 
@@ -77,7 +80,62 @@ def render_words_per_week():
         width = size[0],
         height = size[1])
     dwg.add(dwg.rect(ul, size, fill='ghostwhite'))
-    dwg.add(dwg.polyline([(0, 0), (0, height), (width, height)], fill = 'none', stroke = 'black'))
+    dwg.add(dwg.polyline([(0, 0), (0, height), (width, height)], fill = 'none', stroke = 'black', stroke_opacity = 0.5))
+
+    dwg.add(dwg.text('Words published per week (8wk rolling average)',
+        insert = ((ul[0] + br[0]) / 2, -10),
+        font_family = 'Arial',
+        font_size = 14,
+        text_anchor = 'middle',
+        alignment_baseline = 'middle',
+        fill_opacity = 0.8 ))
+
+    #add nanowrimo legends
+    for amount, label in [(50000 / 30 * 7, 'nanowrimo'), (50000 / 365 * 7, 'nanowriyr?')]:
+        ypos = util_math.remap(0, biggeststat, height, 0, amount)
+        dwg.add(dwg.line((0, ypos), (width, ypos), stroke='black', stroke_opacity=0.15, stroke_dasharray="4 4"))
+        dwg.add(dwg.text(label,
+            insert = (-10, ypos + 3),
+            font_family = 'Arial',
+            font_size = 10,
+            font_style = 'italic',
+            text_anchor = 'end',
+            alignment_baseline = 'middle',
+            fill_opacity = 0.7 ))
+
+    dwg.add(dwg.text('^ nanowriwk (50k/wk)',
+        insert = (-65, -20),
+        font_family = 'Arial',
+        font_size = 10,
+        font_style = 'italic',
+        text_anchor = 'start',
+        alignment_baseline = 'middle',
+        fill_opacity = 0.7 ))
+
+    for amount, label in [(n * 1000, str(n) + "k/wk") for n in range(0, int(biggeststat / 1000) + 1, 5)]:
+        if amount == 0:
+            continue
+        ypos = util_math.remap(0, biggeststat, height, 0, amount)
+        dwg.add(dwg.line((0, ypos), (width, ypos), stroke='black', stroke_opacity=0.1, stroke_dasharray="2 4"))
+        dwg.add(dwg.text(label,
+            insert = (-10, ypos + 3),
+            font_family = 'Arial',
+            font_size = 8,
+            text_anchor = 'end',
+            alignment_baseline = 'middle',
+            fill_opacity = 0.5 ))
+
+    for year in range(xmin.year + 1, xmax.year + 1):
+        xpos = util_math.remap(xmin, xmax, 0, width, datetime.datetime(year, 1, 1))
+        dwg.add(dwg.line((xpos, 0), (xpos, height), stroke='black', stroke_opacity=0.1, stroke_dasharray="2 4"))
+        dwg.add(dwg.text(str(year),
+            insert = (xpos, height + 10),
+            font_family = 'Arial',
+            font_size = 8,
+            text_anchor = 'middle',
+            alignment_baseline = 'hanging',
+            fill_opacity = 0.5 ))
+
     for story, data in storystats:
         line = []
         for date, amount in data:
